bb72283ac6c7cfd2e1099dd5c6923c167110c67b
sergiuplesco <plescosergiu@gmail.com>
2023-06-13 21:13:34 +0300

    next-auth config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppType } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
 
 import { trpc } from "@/utils/trpc";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType<{ session: Session }> = ({
+	Component,
+	pageProps: { session, ...pageProps },
+}) => {
 	return (
-		<>
+		<SessionProvider session={session}>
 			<Component {...pageProps} />
 			<ReactQueryDevtools initialIsOpen={false} />
-		</>
+		</SessionProvider>
 	);
 };
 
