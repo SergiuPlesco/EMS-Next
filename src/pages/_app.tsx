470abf44f9d02b0d6b722a986101d46777c2d720
Iulian Banilean <iulian.banilean@smartdata.solutions>
2023-06-13 14:55:20 +0300

    style(global): normalized default styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
 import type { AppType } from "next/app";
 import { trpc } from "@/utils/trpc";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+`;
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-	return (
-		<>
-			<Component {...pageProps} />
-			<ReactQueryDevtools initialIsOpen={false} />
-		</>
-	);
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
