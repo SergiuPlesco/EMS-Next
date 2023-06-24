b7f1092ff0af13309f710cfe3e0f2dc25ebb9cdd
sergiuplesco <plescosergiu@gmail.com>
2023-06-24 14:45:48 +0300

    next auth trpc prisma config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,12 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
+import { prisma } from "@/server/prisma";
+
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId:
