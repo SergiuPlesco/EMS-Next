647ff6d613dde388ff31ec0a9811d265b609b1af
Sergiu Plesco <sergiu.plesco@smartdata.solutions>
2023-06-27 09:33:10 +0300

    google sign in

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import { prisma } from "@/server/prisma";
@@ -13,39 +12,7 @@ export const authOptions: NextAuthOptions = {
         "53961321690-80tma0e4nk69qeplpd3b38le97uhlghm.apps.googleusercontent.com",
       clientSecret: "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",
     }),
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "smartdata credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize() {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      },
-    }),
   ],
-  // callbacks: {
-  // 	async signIn({ user, account, profile, email, credentials }) {
-  // 		// save to db here
-  // 		return true;
-  // 	},
-  // },
   secret: "sfj46jfg24564dfjgsdfg45", // required in produtction, see next-auth docs
 };
 
