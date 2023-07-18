1b3dbad24f5726d6860dc183ce16ce5d8fd6b3d8
Sergiu Plesco <sergiu.plesco@smartdata.solutions>
2023-07-18 13:27:44 +0300

    fixed session type definition

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,9 +1,10 @@
-import { DefaultSession } from "next-auth";
-
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-    } & DefaultSession["user"];
+      name: string;
+      email: string;
+      image: string;
+    };
   }
 }
