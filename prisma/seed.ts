e71c96f9437e6c01a744da895da9a887e38489b3
Sergiu Plesco <sergiu.plesco@smartdata.solutions>
2023-08-18 14:22:23 +0300

    reset migrations

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-import { POSITIONS } from "../src/constants/common";
+import { POSITIONS, TECHNOLOGIES } from "../src/constants/common";
 const prisma = new PrismaClient();
 
 async function main() {
@@ -8,6 +8,11 @@ async function main() {
     await prisma.position.deleteMany();
     await prisma.position.createMany({
       data: POSITIONS,
+    });
+
+    await prisma.skill.deleteMany();
+    await prisma.skill.createMany({
+      data: TECHNOLOGIES,
     });
   } catch (error) {
     return error;
