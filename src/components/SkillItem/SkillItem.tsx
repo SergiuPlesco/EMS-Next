26f6fe0470585afb3436cd39ede5c7c3c93122eb
sergiuplesco <plescosergiu@gmail.com>
2023-10-01 19:57:58 +0300

    skill item value update

--- src/components/SkillItem/SkillItem.tsx
+++ src/components/SkillItem/SkillItem.tsx
@@ -10,7 +10,7 @@ export interface IUserSkill {
 }
 
 const SkillItem = ({ skill }: { skill: IUserSkill }) => {
-  const [rangeValue, setRangeValue] = useState<number>(skill.rating || 0);
+  const [rangeValue, setRangeValue] = useState<number>(skill.rating || 5);
 
   const updateSkillRating = trpc.users.updateRating.useMutation();
 
@@ -29,15 +29,12 @@ const SkillItem = ({ skill }: { skill: IUserSkill }) => {
           <p>{skill.title}</p>
           <p>{rangeValue}%</p>
         </div>
-        {/* <div className="w-full relative h-[5px] ">
-					<div className="w-1/2 h-full absolute top-0 left-0 z-20 bg-indigo-500 opacity-75" />
-					<div className="w-full h-full absolute top-0 left-0 z-10 bg-indigo-500 opacity-25" />
-				</div> */}
+
         <input
           type="range"
           name=""
           id=""
-          min="0"
+          min="5"
           max="100"
           step="5"
           value={rangeValue}
