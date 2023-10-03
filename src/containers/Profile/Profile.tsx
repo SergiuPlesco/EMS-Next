597cf4c6f77c61ae4edaddde7767c508416e5b99
sergiuplesco <plescosergiu@gmail.com>
2023-10-03 20:01:22 +0300

    created certificates and availability components

--- src/containers/Profile/Profile.tsx
+++ src/containers/Profile/Profile.tsx
@@ -9,6 +9,9 @@ import Tabs from "@/components/Tabs/Tabs";
 import Projects from "@/containers/Projects/Projects";
 import Skills from "@/containers/Skills/Skills";
 
+import Availability from "../Availability/Availability";
+import Certificates from "../Certificates/Certificates";
+
 const tabsElements = [
   {
     label: "Skills",
@@ -17,6 +20,14 @@ const tabsElements = [
   {
     label: "Projects",
     component: <Projects />,
+  },
+  {
+    label: "Availability",
+    component: <Availability />,
+  },
+  {
+    label: "Certificates",
+    component: <Certificates />,
   },
 ];
 
