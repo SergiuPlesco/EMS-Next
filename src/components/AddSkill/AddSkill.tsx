11f860be17518615b420c44ec119ef16e5e97be8
Sergiu Plesco <sergiu.plesco@smartdata.solutions>
2023-10-05 15:22:04 +0300

    notifications

--- src/components/AddSkill/AddSkill.tsx
+++ src/components/AddSkill/AddSkill.tsx
@@ -92,15 +92,31 @@ const AddSkill = () => {
   };
 
   const handleSave = () => {
-    updateSkills.mutate({
-      skills: [
-        ...skills.map((skill) => ({
-          title: skill.title,
-          rating: skill.rating || 0,
-          createdAt: skill.createdAt,
-        })),
-      ],
-    });
+    updateSkills.mutate(
+      {
+        skills: [
+          ...skills.map((skill) => ({
+            title: skill.title,
+            rating: skill.rating || 0,
+            createdAt: skill.createdAt,
+          })),
+        ],
+      },
+      {
+        onSuccess: () => {
+          toast({
+            description: "Skills are updated",
+            variant: "success",
+          });
+        },
+        onError: () => {
+          toast({
+            description: "An error occured, try again",
+            variant: "destructive",
+          });
+        },
+      }
+    );
   };
 
   const handleOpenNewTagForm = () => {
