e685f25b07a5cd0924bbbf305e822b986d07aa0c
Sergiu Plesco <sergiu.plesco@smartdata.solutions>
2024-01-25 11:48:52 +0200

    reset filters, filters name constants

---
+++ src/components/Filters/FiltersReset/FiltersReset.tsx
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+
+import { FILTERS } from "../utils/constans";
+
+const FiltersReset = () => {
+  const { query, pathname, replace } = useRouter();
+  const resetAllFilters = () => {
+    const params = new URLSearchParams(Object(query));
+
+    params.delete(FILTERS.SEARCH);
+    params.delete(FILTERS.AVAILABILITY);
+    params.delete(FILTERS.SKILLS);
+
+    replace(`${pathname}?${params.toString()}`);
+  };
+  return (
+    <Button
+      variant="outline"
+      className="border-[--smart-purple] w-full text-[--smart-purple]"
+      onClick={resetAllFilters}
+    >
+      Reset all filters
+    </Button>
+  );
+};
+
+export default FiltersReset;
--- src/components/Filters/utils/FilterItemWrapper.tsx
+++ src/components/Filters/utils/FilterItemWrapper.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FilterItemWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-start gap-2 items-center mb-2 last-of-type:mb-0">
+    <div className="flex justify-start gap-2 items-start mb-3 last-of-type:mb-0">
       {children}
     </div>
   );
