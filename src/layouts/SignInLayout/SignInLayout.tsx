e3902fe75d2f3a776af9c84d78f0cef57f2bd432
Sergiu Plesco <sergiu.plesco@smartdata.solutions>
2024-03-14 10:36:26 +0200

    removed unnecessary files, set vercel env to emplyt string, add new name in signin layout

--- src/layouts/SignInLayout/SignInLayout.tsx
+++ src/layouts/SignInLayout/SignInLayout.tsx
@@ -5,7 +5,7 @@ import LoginButton from "@/components/LoginButton/LoginButton";
 const SignInLayout = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-4 pt-[5rem] mb-4">
-      <p className="font-bold text-2xl">smartskills</p>
+      <p className="font-bold text-2xl">my team</p>
       <LoginButton />
     </div>
   );
